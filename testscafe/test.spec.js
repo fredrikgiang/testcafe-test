c734a2e1e55a4e46b64f8835a0ee4b3143aafe12
Evelinn Luthander <evelinn@gmail.com>
2022-05-11 09:44:45 +0200

    Repair title test
    Update title string

--- testscafe/test.spec.js
+++ testscafe/test.spec.js
@@ -10,7 +10,7 @@ fixture `Lets go to SwagLabs and test`
 
 /* Run it and correect this test */
 test('Check the title', async t => {
-  await t.expect(Selector('title').innerText).eql('hi')
+  await t.expect(Selector('title').innerText).eql('Swag Labs')
 });
 
 /* Use the locked_out_user and check if you get a error msg */
